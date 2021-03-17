d5c12f2b42834b55352e7806e6d25f7aab1490f7
ZectorJay <74932556+ZectorJay@users.noreply.github.com>
2021-03-18 01:11:11 +0300

    fixed reverse proxy php

--- srcs/mysql/srcs/launch.sh
+++ srcs/mysql/srcs/launch.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 /etc/init.d/telegraf start
 /etc/init.d/mariadb setup
 /etc/init.d/mariadb start
@@ -6,7 +5,8 @@ mysql --user=root -e "CREATE DATABASE wordpress; use wordpress"
 mysql --user=root -e "CREATE DATABASE phpmyadmin; use phpmyadmin" 
 mysql -u root -e "CREATE USER 'Admin'@'%' IDENTIFIED BY 'Admin'"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'Admin'@'%'; FLUSH PRIVILEGES;"
-mysql -u root phpmyadmin < /pma.sql
 mysql -u root wordpress < /wp.sql
-
+mysql -u root phpmyadmin < /pma.sql
+rc-update add mariadb default
 sh usr/share/mariadb/mysql.server start
+tail -f /dev/null
